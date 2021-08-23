f69cd3369a2a5869be2643a8954aa71c8d32fc1a
IURII PAIMURZIN <37944043+ryuriymega@users.noreply.github.com>
2021-08-23 20:52:53 +0300

    Create kladr.sql
    init. SQLite3 database with a new table called kladr

---
+++ kladr.sql
@@ -0,0 +1,1 @@
+create table kladr(deleted varchar(10), name varchar(500), socr varchar(20), code  varchar(20), index1 varchar(100), gninmb varchar(100), uno varchar(100), ocatd  varchar(100), status varchar(100));
